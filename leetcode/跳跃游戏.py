a0303805de91eac5a8c21432b4853475c74c28af
原件 <you@example.com>
2021-03-07 21:29:08 +0800

    fix

--- leetcode/跳跃游戏.py
+++ leetcode/跳跃游戏.py
@@ -1,12 +1,17 @@
 def canJump(nums):
     if 0 not in nums or len(nums) <= 1:
         return True
+
+    if nums[0] == 0:
+        return False
     #最长跳跃数
     cur = 0
     for i in range(len(nums)):
         if i > cur:
             return False
-        cur = max(cur, i+nums[i])
+        cur = max(cur, i + nums[i])
         if cur >= len(nums)-1:
             break
-        return True
+    return True
+
+print(canJump([0,1,0]))
