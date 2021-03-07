f51ed4716c4ea45f63ff7eec42403c79e0ac9ff6
原件 <you@example.com>
2021-03-07 23:50:15 +0800

    add quick_sork.py

---
+++ leetcode/快速排序.py
@@ -0,0 +1,10 @@
+def quick_sort(array):
+    if len(array) < 2:
+        return array
+
+    middle = array[0]
+    left = [i for i in array[1::] if i <= middle ]
+    right = [i for i in array[1::] if i > middle ]
+    return quick_sort(left) + [middle] + quick_sort(right)
+
+print(quick_sort([2,5,4,8,1]))
