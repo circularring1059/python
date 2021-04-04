8a1aee93e165f5df079110e2c7b9edb2b454cecb
原件 <you@example.com>
2021-04-04 20:23:41 +0800

    update

--- regex/day1.py
+++ regex/day1.py
@@ -13,8 +13,11 @@ print(re.match(patter1, name))  #<re.Match object; span=(0, 6), match='circul'>
 
 print(re.match("cir", name)) #<re.Match object; span=(0, 3), match='cir'>
 
-#search 开始没有匹配上可以往下匹配
+#search 开始没有匹配上可以往下匹配,匹配到一次就停止
 print(re.search(pattern, name)) # <re.Match object; span=(12, 16), match='1010'>
 res = re.search(pattern,name)
 print("*" * 8)
 print(res.span(),res.group())  #span() 返回iddex位置 group()返回匹配值
+
+#findall  可以多次匹配，返回列表
+print(re.findall("ring", "circularring1010circularring1010")) #['ring', 'ring']
