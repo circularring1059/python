a11fad2ac83f1e6fa3202e72d1fa52af26bb3d9e
原件 <you@example.com>
2021-04-05 17:49:51 +0800

    update

--- regex/day4.py
+++ regex/day4.py
@@ -15,12 +15,14 @@ print(re.match(r"<[a-bA-Z]*>(.+)", msg)) # None  前面不匹配
 print(re.match(r"<[a-zA-Z]*>.+<[a-zA-Z]*/>", msg)) # <re.Match object; span=(0, 24), match='<html>hello world<html/>'
 print(re.match(r"<[a-zA-Z]*>(.+)<[a-zA-Z]*/>", msg)) # <re.Match object; span=(0, 24), match='<html>hello world<html/>'
 
+print(re.match(r"<[a-zA-Z]*>(.+)<[a-bA-Z]*/>", msg), "****") # None  后面不匹配
+
 msg1 = "<html>hello world<html/><br/>"
 print(re.match(r"<[a-zA-Z]*>(.+)<[a-zA-Z]*/>", msg1)) # <re.Match object; span=(0, 29), match='<html>hello world<html/><br/>'>
 
 msg2 = "<html><span>hello world<html/><span/>"
 
-res = re.match(r"<([a-z]*)><([a-z]*)>(.+)<(\1/)><(\2/)>", msg2)  # ()代表一组，group(1) 表示一组
+res = re.match(r"<([a-z]*)><([a-z]*)>(.+)<(\1/)><(\2/)>", msg2)  # ()代表一组，group(1) 表示一组 \1表示引用第一组
 print(res) #<re.Match object; span=(0, 37), match='<html><span>hello world<html/><span/>'>
 print(res.group(1))  #html
 print(res.group(2))  #span
