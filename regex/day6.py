bb2e7a76eba3f5e6a832d426796cc07439fc1005
原件 <you@example.com>
2021-04-05 21:00:09 +0800

    defferce between str.split and re.replit

--- regex/day6.py
+++ regex/day6.py
@@ -4,7 +4,7 @@ sub
 spilt
 """
 
-name = "circularri  ng1010circularring1010"
+name = "circularri   ng1010circularring1010"
 
 print(re.sub(r"\d+", "yiling", name))  #把1010 换成yilingyiling
 print(re.sub(r"\d", "yiling", name))  #把1 0 f 分别换成ylling
@@ -20,3 +20,6 @@ print(re.sub(r"\d+", add, name))  #1010  加 1 变成1011
 
 print(re.split(r"[l,0]", name))  #['circu', 'arring1', '1', 'circu', 'arring1', '1', '']  切割
 print(re.split(r"[l, 0]", name)) #['circu', 'arri', '', 'ng1', '1', 'circu', 'arring1', '1', '']     这里一个空格，需要注意区别
+
+print(name.split(" "))  #只按一个切割
+print(name.split())  #默认空格，多个空格都按一个切割
