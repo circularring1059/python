c815c2cde9ce964e6d30132f665687974546bdb9
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-04-06 20:45:59 +0800

    update

--- leetcode/电话号码字母组合.py
+++ leetcode/电话号码字母组合.py
@@ -10,7 +10,7 @@ def phoneLetter(digits):
         "6": "mno",
         "7": "pqrs",
         "8": "tuv",
-        "9": "wzyz"
+        "9": "wxyz"
     }
 
     res  = []
@@ -31,3 +31,31 @@ def phoneLetter(digits):
 
 
 print(phoneLetter("293"))
+
+
+def letterCombinations(digits):
+    if len(digits) == 0:
+        return []
+
+    keyboard = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    }
+
+    res = []
+    def find_com_str(index, s):
+        if index == len(digits):
+            res.append(s)
+            return
+        for c in keyboard[digits[index]]:
+            find_com_str(index+1, s+c)
+    find_com_str(0, "")
+    return res
+
+print(letterCombinations("23"))
