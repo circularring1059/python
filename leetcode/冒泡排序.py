e2ede4b78a90576d4a6824f1d6504f61939e8e26
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-08-31 10:41:15 +0800

    update

--- leetcode/冒泡排序.py
+++ leetcode/冒泡排序.py
@@ -4,3 +4,23 @@ def bubbling_sort(arg):
             if arg[y] > arg[y + 1]:
                 arg[y], arg[y + 1] = arg[y + 1], arg[y]
     return arg
+
+#优化  设置一个flag  在某轮排序后 falg 仍然为 True， 说明无需再排序了
+
+class BubblingSort():
+    def __init__(self, array):
+        self.array = array
+
+    def bubbling_sort(self):
+        for i in range(len(self.array)-1):
+            flag =True
+            for j in range(len(self.array)-1-i):
+                if self.array[j] > self.array[j+1]:
+                    self.array[j], self.array[j+1] = self.array[j+1], self.array[j]
+                    flag = False
+            if flag:
+                    break
+        return self.array
+
+bubbling_sort_ins = BubblingSort([2,5,2,9,6])
+print(bubbling_sort_ins.bubbling_sort())
