4fe10de042d2f57b37026d133fa1ea0b498f93a3
原件 <ring@163.com>
2021-09-06 20:55:16 +0800

    update

--- leetcode/不同路径.py
+++ leetcode/不同路径.py
@@ -1,17 +1,55 @@
-class Solution():
-    def uniquePath(self, m, n):
-        k = m + n -2
-        t = m-1
+# class Solution():
+#     def uniquePath(self, m, n):
+#         k = m + n -2
+#         t = m-1
+#
+#         up = 1
+#         for i in range(0, t):
+#             up *= k-i
+#
+#         down = 1
+#         for i in range(1, m):
+#             down *= 1
+#         return up // down
+#
+# soultion_ins = Solution()
+#
+# print(soultion_ins.uniquePath(4,4))
+# print("*")
+#
+# def maxPath(m, n):
+#     res = []
+#     def s(left=1 ,right=1):
+#         # print(left, right)
+#         if left == m and right == n:
+#             res.append("*")
+#
+#         if left < m:
+#             s(left+1, right)
+#
+#         if right < n:
+#             s(left, right+1)
+#
+#
+#     s()
+#     return len(res)
+#
+# print(maxPath(4,4))
+#
+#
 
-        up = 1
-        for i in range(0, t):
-            up *= k-i
 
-        down = 1
-        for i in range(1, m):
-            down *= 1
-        return up // down
+def getAns(x=0,y=0, m=0, n=0, num=0):
+    if (x == m and y == n):
+        return 1
+    n1 = 0
+    n2 = 0
+    if (x + 1 <= m):
+        n1 = getAns(x + 1, y, m, n, num)
 
-soultion_ins = Solution()
+    if (y + 1 <= n):
+        n2 = getAns(x, y + 1, m, n, num)
+    return n1 + n2
 
-print(soultion_ins.uniquePath(2,2))
+print(getAns(m=2, n=2))
+
--- leetcode/生成括号.py
+++ leetcode/生成括号.py
@@ -2,15 +2,18 @@ def generateParenthesis(n):
     res = []
     def  backtrack(s ='', left = 0, right = 0):
         if len(s) == 2 * n :
+            print(s)
             res.append(s)
             return res
 
         if left < n:
+            print("*", s, left)
             backtrack(s + "(", left + 1, right)
 
-        if left > right:
+        if right < n:
+            print("**", s,right)
             backtrack(s + ")", left, right+1)
     backtrack()
     return res
 
-print(generateParenthesis(1))
+print(generateParenthesis(4))
