135d10ba0fa7a4d8f65d1067062b33a57211654b
原件 <ring@163.com>
2021-09-07 13:09:40 +0800

    update

--- leetcode/不同路径.py
+++ leetcode/不同路径.py
@@ -1,24 +1,27 @@
 def maxPath(m, n):
     # res = []
     count = 0
-    def backtask(left=0 ,right=0):
+
+    def backtask(left=0, right=0):
         # print(left, right)
         nonlocal count
         if left == m and right == n:
             count += 1
         else:
             if left < m:
-                backtask(left+1, right)
+                backtask(left + 1, right)
 
             if right < n:
-                backtask(left, right+1)
+                backtask(left, right + 1)
+
     backtask()
     return count
 
-print(maxPath(5,5))
+
+print(maxPath(5, 5))
 
 
-def getAns(x=0,y=0, m=0, n=0, num=0):
+def getAns(x=0, y=0, m=0, n=0, num=0):
     if (x == m and y == n):
         return 1
     n1 = 0
@@ -29,5 +32,6 @@ def getAns(x=0,y=0, m=0, n=0, num=0):
     if (y + 1 <= n):
         n2 = getAns(x, y + 1, m, n, num)
     return n1 + n2
+
 
 print(getAns(m=5, n=5))
