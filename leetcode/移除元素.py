4eb9957eb370a70e5e1b73c132177af87b6fd01c
原件 <ring@163.com>
2021-09-08 20:14:55 +0800

    update

--- leetcode/移除元素.py
+++ leetcode/移除元素.py
@@ -10,3 +10,25 @@ def removeElement(array, val):
     return len(array)
 
 print(removeElement([1,234,45,4,5,4], 4))
+
+def removeElement1(array, val):
+    if not array:
+        return 0
+
+    i = 0
+    for j in range(len(array)):
+        if array[j] != val:
+            array[i] = array[j]
+            i += 1
+    return i
+
+print(removeElement1([1,2,4,4,5,2,1], 1))
+
+def removeElement2(array, val):
+    for i in range(len(array)-1, -1, -1):
+       if array[i] == val:
+           array.pop()
+    return len(array)
+
+
+print(removeElement2([1,2,3,3,4],3))
