c301c4f012eb2e6248b1e3a9434997533d886164
原件 <ring@163.com>
2021-09-11 16:45:49 +0800

    update

--- leetcode/接雨水.py
+++ leetcode/接雨水.py
@@ -21,7 +21,6 @@ class Solution():
         max_left[0] = array[0]
         max_right[len(array)-1] = array[len(array)-1]
         for i in range(1, len(array)):
-            print("*", i)
             max_left[i] = max(array[i], max_left[i-1])
 
         for j in range(len(array)-2, -1, -1):
@@ -32,8 +31,26 @@ class Solution():
                 res += min(max_left[i], max_right[i]) - array[i]
         return res
 
+    def trap2(self, array):
+        res = 0
+        left, right = 0, len(array) -1
+        max_left = array[0]
+        max_right = array[len(array)-1]
+        while left <= right:
+            max_left = max(array[left], max_left)
+            max_right = max(array[right], max_right)
+
+            if max_left < max_right:
+                res += max_left - array[left]
+                left += 1
+            else:
+                res += max_right - array[right]
+                right -= 1
+
+        return res
 
 
 soution_ins = Solution()
-# print(soution_ins.trap([0,1,0,2]))
+print(soution_ins.trap([0,1,0,2]))
 print(soution_ins.trap1([0,1,0,2]))
+print(soution_ins.trap2([0,1,0,2]))
