7e158503045c3d917874710d46bfb86be8601bc1
原件 <ring@163.com>
2021-09-11 16:06:58 +0800

    update

--- leetcode/接雨水.py
+++ leetcode/接雨水.py
@@ -1,35 +1,39 @@
 class Solution():
 
     def trap(self, array):
+        res = 0
+        for i in range(len(array)):
+            max_left, max_right = 0, 0
+            for j in range(i):
+                max_left = max(max_left, array[j])
 
-        if not array:
-            return "invald input"
-        s = 0
-        x = 0
-        for i in range(len(array)-1):
-          if array[i] != 0:
-              t = i
-              for j in range(t, len(array)-1):
-                  if array[j+1] >= array[j]:
-                      s = s + array[j]
-                  else:
-                      for x in range(j+2, len(array)-1):
-                          if array[x] > array[j]:
-                              s = s+ array[j] * (x-j)
-                          elif array[x] == array[j]:
-                              s = s+ array[j] * (x-j)
-                          else:
-                              pass
-                              # s = s+  max(array[x+1], len(array)-1]) * rfind(max(array[x+1], len(array)-1) )
-              break
-#
-#
+            for j in range(i, len(array)):
+                max_right = max(max_right, array[j])
 
-for i in range(10):
-    if i == 3:
-        i == 7
+            if min(max_left, max_right) > array[i]:
+                res += min(max_left, max_right) - array[i]
+        return res
 
-    print(i)
+    def trap1(self, array):
+        res =0
+        max_left = [0] * len(array)
+        max_right = [0] * len(array)
+        max_left[0] = array[0]
+        max_right[len(array)-1] = array[len(array)-1]
+        for i in range(1, len(array)):
+            print("*", i)
+            max_left[i] = max(array[i], max_left[i-1])
+
+        for j in range(len(array)-2, -1, -1):
+            max_right[j] = max(array[j], max_right[j+1])
+
+        for i in range(len(array)):
+            if min(max_left[i], max_right[i]) > array[i]:
+                res += min(max_left[i], max_right[i]) - array[i]
+        return res
 
 
 
+soution_ins = Solution()
+# print(soution_ins.trap([0,1,0,2]))
+print(soution_ins.trap1([0,1,0,2]))
