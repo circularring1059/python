384aee35275b26b4d5746d94a0b59f99a5f5ff26
原件 <ring@163.com>
2021-09-14 20:26:54 +0800

    update

--- leetcode/第三大的数.py
+++ leetcode/第三大的数.py
@@ -19,7 +19,7 @@ class Solution():
             res = max(i, res)
         return res
 
-    def  thridMax2(self, nums):
+    def thridMax2(self, nums):
         nums = list(set(nums))
         if len(nums) <= 2:
             return max(nums)
@@ -31,7 +31,21 @@ class Solution():
             nums[i],nums[s] = nums[s],nums[i]
         return nums[2]
 
+    # def thridMax3(self, nums):
+    #     nums = list(set(nums))
+    #     if len(nums) <= 2:
+    #         return max(nums)
+    #     def backtask(nums):
+    #         if len(nums) <= 1:
+    #             return nums
+    #         mid = nums[0]
+    #         left = [i for i in range(1,len(nums)) if nums[i] <= mid ]
+    #         right = [i for i in range(1,len(nums)) if nums[i] > mid]
+    #         return backtask(right)+[mid]+backtask(left)
+    #     s=backtask(nums)
+    #     return s
 
 thrid_max_ins = Solution()
 print(thrid_max_ins.thridMax([2, 5, 6, 7]))
 print(thrid_max_ins.thridMax2([2, 5, 6, 7]))
+print(thrid_max_ins.thridMax3([2, 5, 6, 7]))
