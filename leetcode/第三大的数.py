1ede68ce39d3b08c8659120890ebd0cb50f1631b
原件 <ring@163.com>
2021-09-16 20:03:52 +0800

    update

--- leetcode/第三大的数.py
+++ leetcode/第三大的数.py
@@ -29,6 +29,8 @@ class Solution():
                 if nums[j] > nums[s]:
                     s = j
             nums[i],nums[s] = nums[s],nums[i]
+            if i == 2:
+                break
         return nums[2]
 
     def thridMax3(self, nums):
@@ -44,8 +46,38 @@ class Solution():
             return backtask(right)+[mid]+backtask(left)
         return backtask(nums)[2]
 
+    def thridMax4(self, nums):
+        nums = list(set(nums))
+        if len(nums) <= 2:
+            return max(nums)
+
+        for i in range(len(nums)-1):
+            for j in range(len(nums)-i-1):
+                flag = True
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+                    flag = False
+            if flag or i == 2:
+                break
+        return nums[len(nums)-3]
+        # return nums
+
 
 thrid_max_ins = Solution()
 print(thrid_max_ins.thridMax([2, 5, 6, 7]))
 print(thrid_max_ins.thridMax2([2, 5, 6, 7]))
 print(thrid_max_ins.thridMax3([2, 5, 6, 7]))
+print(thrid_max_ins.thridMax4([2, 5, 6, 7]))
+
+# def bu(array):
+#     for i in range(len(array)-1):
+#         flag = True
+#         for j in range(len(array)-i-1):
+#             if array[j] > array[j+1]:
+#                 array[j], array[j+1] = array[j+1], array[j]
+#                 flag = False
+#         if flag or i == 2:
+#             break
+#     return array
+#
+# print(bu([1,8,3,5,2,9,2,1]))
