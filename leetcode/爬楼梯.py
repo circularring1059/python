2fb77d5d55d4c6f6c4b62bd4620d5550adc216db
原件 <ring@163.com>
2021-09-19 15:16:22 +0800

    update

--- leetcode/爬楼梯.py
+++ leetcode/爬楼梯.py
@@ -1,17 +1,32 @@
 class Solution():
-     def climbStairs(self,n):
-         if n <= 2:
-             return [1,2][n-1]
-         first = 1
-         second = 2
-         cnt = 2
-         while cnt < n:
-             cnt += 1
-             cur = first + second
-             if cnt == n:
+    def climbStairs(self, n):
+        if n <= 2:
+            return [1, 2][n - 1]
+        first = 1
+        second = 2
+        cnt = 2
+        while cnt < n:
+            cnt += 1
+            cur = first + second
+            if cnt == n:
                 return cur
-             first = second
-             senond = cur
+            first = second
+            second = cur
+
+    def climbStairs1(self, n):
+        def backstack(n):
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+
+            return backstack(n -1) + backstack(n -2)
+        return backstack(n)
+
+
+
+
 
 climb_stairs_ins = Solution()
 print(climb_stairs_ins.climbStairs(4))
+print(climb_stairs_ins.climbStairs1(4))
