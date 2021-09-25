b4cb13d657a0ded97de93ffffc0171fc9733ddaf
原件 <ring@163.com>
2021-09-25 16:13:52 +0800

    update

--- leetcode/跳跃游戏.py
+++ leetcode/跳跃游戏.py
@@ -4,17 +4,18 @@ def canJump(nums):
 
     if nums[0] == 0:
         return False
-    #最长跳跃数
+    # 最长跳跃数
     cur = 0
     for i in range(len(nums)):
         if i > cur:
             return False
         cur = max(cur, i + nums[i])
-        if cur >= len(nums)-1:
+        if cur >= len(nums) - 1:
             break
     return True
 
-print(canJump([0,1,0]))
+
+print(canJump([0, 1, 0]))
 
 
 class CanJump():
@@ -28,16 +29,34 @@ class CanJump():
         if self.array[0] == 0:
             return False
 
-        #在某点能跳跃最大长度,开始是起始位置
+        # 在某点能跳跃最大长度,开始是起始位置
         max_length = self.array[0]
-        #肯定可以跳到第二个，所以从第二个点开始循环
-        for i in range(1, len(self.array)-1):
-            max_length = max(max_length, self.array[i] +i)
-            if max_length >= len(self.array)-1:
+        # 肯定可以跳到第二个，所以从第二个点开始循环
+        for i in range(1, len(self.array) - 1):
+            max_length = max(max_length, self.array[i] + i)
+            if max_length >= len(self.array) - 1:
                 return True
-            #判断能否到下一跳
-            if max_length < i+1:
+            # 判断能否到下一跳
+            if max_length < i + 1:
                 return False
 
-can_jump_ins =  CanJump([1,1,0,2,0])
+    def can_jump1(self):
+        if 0 not in self.array or len(self.array) <= 1:
+            return True
+
+        if self.array[0] == 0:
+            return False
+
+        max_length = max(self.array[0], self.array[1])
+        # 肯定可以跳到第二个，所以从第三个点开始循环
+        for i in range(1, len(self.array) - 1):
+            if max_length < i:
+                return False
+            max_length = max(max_length, self.array[i] + i)
+            if max_length >= len(self.array) - 1:
+                return True
+
+
+can_jump_ins = CanJump([1, 1, 0, 2, 0])
 print(can_jump_ins.can_jump())
+print(can_jump_ins.can_jump1())
