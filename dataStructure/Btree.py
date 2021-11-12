b0e53d478db97d9d19dbadb2e69979332b4e4106
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-11-12 15:44:21 +0800

    update

--- dataStructure/Btree.py
+++ dataStructure/Btree.py
@@ -30,6 +30,49 @@ class BTree():
         if self.data is not None:
             print(self.data)
 
+    #树高
+    def high(self):
+        if self.data is None:
+            return 0
+        elif self.left is None  and self.right is None:
+            return 1
+        elif self.left is None  and self.right is not None:
+            return 1 + self.right.high()
+        elif self.left is not None and self.right is None:
+            return 1 + self.left.high()
+        else:
+            return 1 + max(self.left.high(), self.right.high())
+
+    #叶子节点
+    def leaves(self):
+        if self.data is None:
+            return None
+        elif self.left is None and self.right is None:
+            print(self.data, end="")
+        elif self.left is None and self.right is not None:
+            self.right.leaves()
+        elif self.left is not None and self.right is None:
+            self.left.leaves()
+        else:
+            self.left.leaves()
+            self.right.leaves()
+
+
+    def levelorder(self):
+        #左孩子
+        def LChild_Of_Node(node):
+            return node.left if node.left else None
+        #右孩子
+        def RChild_Of_Node(node):
+            return node.right if node.right else None
+        #层序遍历列表
+        level_order  = []
+        if self.data:
+            level_order.append([self])
+
+        #高度
+        high = self.high()
+
 
 
 
