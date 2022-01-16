213a256b075bc0c0664089fe7da27685b624672f
原件 <ring@163.com>
2022-01-16 17:52:11 +0800

    update

--- leetcode/最长子串.py
+++ leetcode/最长子串.py
@@ -10,4 +10,27 @@ def lengthOfLongSubstring(str):
         usedChar[str[i]] = i
     print(usedChar)
     return max_len
-print(lengthOfLongSubstring("abcbdf "))
+print(lengthOfLongSubstring("abcabcbb"))
+
+
+def lengthOfLongSubstring1(str):
+    max_len = 0
+    dict1 = {}
+    start = 0
+    for i in range(len(str)):
+        if str[i] not in dict1:
+            dict1[str[i]] = i
+        else:
+            max_len = max(max_len, i-start)
+            if dict1[str[i]] > start:
+                dict1[str[i]] = i
+                start = i
+            else:
+                start = dict1[str[i]] + 1
+                dict1[str[i]] = i
+
+    max_len = max(max_len, len(str)-start)
+
+    return max_len
+
+print(lengthOfLongSubstring1("abcabcbb"))
