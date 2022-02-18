9267dc7babfcc643bc8ccb61d609f43ea2d4005f
yuanjiancheng <yuan.jiancheng@ricequant.com>
2022-02-18 17:47:34 +0800

    update

--- process&&thread/day7.py
+++ process&&thread/day7.py
@@ -2,17 +2,19 @@ import os
 from multiprocessing import Pool
 from time import sleep
 
+
 def task1(parameter):
     sleep(1)
     print("this is task:{}".format(parameter), "pid:{}".format(os.getpid()))
 
+
 if __name__ == '__main__':
     pool = Pool(3)
 
-    tasks = ["listing", "watching","running", "jumping"]
+    tasks = ["listing", "watching", "running", "jumping"]
 
     for task in tasks:
-        pool.apply(task1, args=(task,))  #阻塞，但是进程号可以复用，没有callback
+        pool.apply(task1, args=(task,))  # 阻塞，但是进程号可以复用，没有callback
 
     pool.close()
     pool.join()
--- process&&thread/day8.py
+++ process&&thread/day8.py
@@ -1,20 +1,23 @@
-#进程间通行，可以使用queue
+# 进程间通行，可以使用queue
 from multiprocessing import Process, Queue
 from time import sleep
 import random
 
 
-def  product(q):
+def product(q):
     # listA = ["A", "B", "C", "D"]
     for i in range(5):
-        q.put(chr(random.randint(65, 90)), timeout=5)
+        print("put: ")
+        q.put(chr(random.randint(65, 90)), )  # full 后阻塞
+        # q.put(chr(random.randint(65, 90)), timeout=5)
         # sleep(1)
+
 
 def consume(q):
     while True:
         sleep(2)
-        print(q.get())
-        # print(q.get(timeout=3))
+        # print(q.get())   #empty  阻塞
+        print(q.get(timeout=3))  # 超时后退出
 
 
 if __name__ == '__main__':
