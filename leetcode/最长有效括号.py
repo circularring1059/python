b55845da90b192a933c2a0cfccff4896074fb184
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-26 11:50:59 +0800

    update

--- leetcode/最长有效括号.py
+++ leetcode/最长有效括号.py
@@ -49,3 +49,15 @@ def Solution(str):
 
 print(Solution("()(())(("))
 print(Solution("(((())))(()"))
+
+# def Solution1(str):
+#     lens = len(str)
+#     dp = [0 for _ in range(lens+1)]
+#     for i in range(2, lens):
+#         if str[i] == ")" and str[i-dp[i-1] -1] == "(":
+#             dp[i] = dp[i-1] + dp[i-dp[i-1]-2] +2
+#     return dp[-1]
+# print(Solution1("(((())))(()"))
+
+
+
