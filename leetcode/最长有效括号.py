80cd0b1a37a27d58c1ca2981016d2efb4c76b4af
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-26 10:38:25 +0800

    update

--- leetcode/最长有效括号.py
+++ leetcode/最长有效括号.py
@@ -13,12 +13,14 @@ def longestValidParentheses(arg):
                 stack.pop()
                 if len(stack):
                     lens = i - stack[-1]
-                    if lens > max_len:
-                        max_len = lens
+                    # if lens > max_len:
+                    #     max_len = lens
+                    max_len = max(max_len, lens)
                 else:
                     lens = i + 1
-                    if lens > max_len:
-                        max_len = lens
+                    # if lens > max_len:
+                    #     max_len = lens
+                    max_len = max(max_len, lens)
             else:
                 stack.append(i)
         i += 1
@@ -26,3 +28,24 @@ def longestValidParentheses(arg):
 
 
 print(longestValidParentheses("(((())))(()"))
+
+
+def Solution(str):
+    if len(str) <= 1:
+        return 0
+    stack = [0]
+    max_len = 0
+    for i in str:
+        if i == "(":
+            stack.append(0)
+        else:
+            if len(stack) > 1:
+                val = stack.pop()
+                stack[-1] += val +2
+                max_len = max(max_len, stack[-1])
+            else:
+                stack = [0]
+    return max_len
+
+print(Solution("()(())(("))
+print(Solution("(((())))(()"))
