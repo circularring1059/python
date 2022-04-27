fbfaa39cc5b7296e1895b7ffaad1f28a77855995
元件 <56504340+circularring1059@users.noreply.github.com>
2022-04-27 11:03:25 +0800

    update

---
+++ leetcode/能否跳跃.py
@@ -0,0 +1,24 @@
+def canJump(nums):
+    if  len(nums) <= 1:
+        return True
+
+    if nums[0] == 0:
+        return False
+
+    max_jump = nums[0]
+
+    #从第二点开始到倒数第个二点
+    for i in range(1,len(nums)-1):
+        print(i)
+        #先判断是否可以跳到该点
+        if max_jump < i:
+            return False
+        #在该点能跳的最大长度
+        max_jump = max(max_jump, i+nums[i])
+        if max_jump >= len(nums)-1:
+            return True
+
+    return False
+
+
+print(canJump([1,0,1]))
